fe682de86ff2faa5dcc7e965f1623abcc5777774
kerimozgur <115617325+kerimozgur@users.noreply.github.com>
2022-10-19 14:53:15 +0300

    Add files via upload

---
+++ user/src/main/java/com/wb/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.wb.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping(value = "/hello")
+    public String getHello() {
+        return "Hello";
+    }
+}
